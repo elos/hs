b057c65942a674ebdaadcd578a261262250b8ea0
Sam Calvert <sam.clvrt@gmail.com>
2016-06-14 23:08:04 -0700

    Collapse let binding

--- src/Elos/Config.hs
+++ src/Elos/Config.hs
@@ -27,8 +27,7 @@ defaultConfigPath = fmap (++ "/elosconfig.json") getHomeDirectory
 loadConfig :: FilePath -> IO Config
 loadConfig fp = do
     configFile <- B.readFile fp
-    let config = decode configFile
-    case config of
+    case decode configFile of
         Nothing -> fail $ "could not parse " ++ fp
-        Just c -> return c
+        Just config -> return config
 
