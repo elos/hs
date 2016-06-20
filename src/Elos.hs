f689f3b3dedba544667becb6fd1f1615a95596ec
Sam Calvert <sam.clvrt@gmail.com>
2016-06-19 23:39:57 -0700

    Remove unused imports from Elos.hs

--- src/Elos.hs
+++ src/Elos.hs
@@ -4,19 +4,8 @@ module Elos (
     elosRunApp
     ) where
 
-import Elos.Autonomous
 import Elos.Config
-import qualified Elos.DB as DB
-import Elos.Models
-
-import Control.Concurrent (forkIO)
-import Control.Monad (forever, void)
-import Control.Monad.Trans (liftIO)
 import qualified Data.ByteString.Char8 as B
-import Data.Text (Text)
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-
 import qualified Network.WebSockets as WS
 
 authRoute :: String -> String -> String
