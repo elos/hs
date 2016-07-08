c0bee03da247a7c62dc813f8b4492d7c7944e972
Sam Calvert <sam.clvrt@gmail.com>
2016-07-07 22:31:02 -0700

    Be smarter about reading WebSocket

--- src/Elos/Autonomous.hs
+++ src/Elos/Autonomous.hs
@@ -6,7 +6,7 @@ module Elos.Autonomous (
 
 import Elos.DB (Change)
 
-import Control.Monad (forever)
+import Control.Monad (forever, void)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Reader (ReaderT (runReaderT), ask)
@@ -20,21 +20,24 @@ import qualified Network.WebSockets as WS
 type WebsocketsT = ReaderT WS.Connection
 type Agent = Sink Change (WebsocketsT IO) ()
 
-wsSource :: (MonadIO m, WS.WebSocketsData a) => Source (WebsocketsT m) a
-wsSource = do
+wsInOnce :: (MonadIO m, WS.WebSocketsData a) => Source (WebsocketsT m) a
+wsInOnce = do
     conn <- lift ask
     x <- liftIO $ WS.receiveData conn
     yield x
 
+wsIn :: (MonadIO m, WS.WebSocketsData a) => Source (WebsocketsT m) a
+wsIn = wsInOnce >> wsIn
+
 changes :: (MonadIO m) => Source (WebsocketsT m) Change
-changes = wsSource =$= jsonConduit
+changes = wsIn =$= jsonConduit
 
 jsonConduit :: (Monad m, FromJSON a) => Conduit B.ByteString m a
 jsonConduit = CL.mapMaybe decode
 
 agentApp :: [Agent] -> WS.ClientApp ()
-agentApp agents conn = forever $ runReaderT (changes $$ agentsSink) conn
-    where agentsSink = sequenceSinks agents
+agentApp agents conn = runReaderT (changes $$ agentsSink) conn
+    where agentsSink = void $ sequenceSinks agents
 
 listenForChange :: (Change -> IO ()) -> Agent
 listenForChange f = awaitForever (liftIO . f)
